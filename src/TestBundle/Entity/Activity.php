403eea68c63d8ebf54751e6e285683d415a67e00
jamarboledas <jamarboledas@gmail.com>
2016-02-19 19:32:33 +0100

    'Activity' and 'SubActivity' entities have been created

--- src/TestBundle/Entity/Activity.php
+++ src/TestBundle/Entity/Activity.php
@@ -5,26 +5,27 @@ namespace TestBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Program
+ * Activity
  *
  * @ORM\Table()
  * @ORM\Entity()
- * 
+ *
  */
-class Program
+class Activity
 {
     /**
      * @var int
      *
-     * @ORM\Column(type="integer")
+     * @ORM\Column(name="id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
 
     /**
-    *   @var string
-    *   @ORM\Column(type="string", nullable=false)
-    */
+     *   @var string
+     *   @ORM\Column(type="string", nullable=false)
+     *
+     */
     private $name;
 }
