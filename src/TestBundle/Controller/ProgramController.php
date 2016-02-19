64f7232ea30e06fdf2b82f24e7306e8a1679b026
jamarboledas <jamarboledas@gmail.com>
2016-02-19 20:18:05 +0100

    it's been prepared to generate lists

---
+++ src/TestBundle/Controller/ProgramController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Class ProgramController
+ * @package TestBundle\Controller
+ * @Route("/program")
+ */
+class ProgramController extends Controller
+{
+    /**
+     * @Route("/list")
+     */
+    public function indexAction()
+    {
+        
+    }
+}
